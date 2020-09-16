90db3b2baf0f162d3bcf7123632ba9e4b7c5b9be
Ray Epps <rayharryepps@gmail.com>
2020-09-15 22:42:00 -0600

    add debug

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ function parse_version(str) {
 
 async function get_version_at_commit(owner, repo, hash) {
   const version_url = `https://raw.githubusercontent.com/${owner}/${repo}/${hash}/${file_path}`
+  core.debug(`Pulling version from ${version_url}`)
   try {
     const { response, body } = await http_get(version_url)
     return parse_version(body)
